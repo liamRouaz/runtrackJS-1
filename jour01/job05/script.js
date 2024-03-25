1bc0c5d52e670167647ad0d1d655029abe23510e
liam-rouaz <145978285+liamRouaz@users.noreply.github.com>
2024-03-25 21:52:48 +0100

    Delete jour01 directory

--- jour01/job05/script.js
+++
@@ -1,9 +0,0 @@
-var joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-
-function afficherJoursSemaines() {
-    for (let i = 0; i < joursSemaines.length; i++) {
-        console.log(joursSemaines[i]);
-    }
-}
-
-afficherJoursSemaines();
